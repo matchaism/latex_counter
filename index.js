40175032e375b156fba0f9dda1fea2409a70eeec
matchaism <60777705+matchaism@users.noreply.github.com>
2025-01-17 01:29:27 +0900

    change for gh-pages

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function loadCharacterCount() {
     // データを表示
     document.getElementById("result").textContent = `Previous Count: ${data.previous_count}, Current Count: ${data.current_count} (diff: ${data.difference})`;
     // ポスト(ツイート)の用意
-    const target = document.getElementById("tweet").href = "https://twitter.com/intent/tweet?text=" + `論文の文字数${data.previous_count}->${data.current_count} ${location.href}`;
+    const target = document.getElementById("tweet").href = "https://twitter.com/intent/tweet?text=" + `matchaismの修論の文字数${data.previous_count}->${data.current_count} ${location.href}`;
   } catch (error) {
     console.error(error);
     document.getElementById("result").textContent = "データの取得に失敗しました。";
